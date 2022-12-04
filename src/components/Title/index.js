b25dd85e6c3c81baf96df4954bd071ff84735ec5
Fatima <fzms90@gmail.com>
2022-12-04 11:02:09 +0000

    title changed

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -3,7 +3,7 @@ import './title.css';
 const Title = () => {
     return (
         <>
-            <div className="title text-center fs-2 fw-bolder pt-4 pe-5 font-family-Android7">ROBOTS STORE</div>
+            <div className="title text-center fs-2 fw-bolder pt-4 pe-5 font-family-Android7">ROBOT STORES</div>
         </>
     );
 }
