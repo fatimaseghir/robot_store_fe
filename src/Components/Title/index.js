f333215a0de916d367e91ca8aefd70e88e5a0d31
Fatima <fzms90@gmail.com>
2022-12-08 15:54:41 +0000

    title - link to home page

--- src/Components/Title/index.js
+++ src/Components/Title/index.js
@@ -1,9 +1,10 @@
 import './style.css';
+import {Link} from "react-router-dom";
 
 const Title = () => {
     return (
         <>
-            <div className="title text-center fs-2 fw-bolder pt-4 pe-5 font-family-Android7">ROBOT STORES</div>
+            <Link to={`/`}> <div className="title text-center fs-2 fw-bolder pt-4 pe-5 font-family-Android7">ROBOT STORES</div></Link>
         </>
     );
 }
